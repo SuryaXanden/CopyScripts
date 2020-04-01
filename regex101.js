d2ff7109391a4cda3a9a51260498717532862528
Surya U <suryaxanden@gmail.com>
2020-04-01 09:26:57 +0530

    Update and rename regex101 to regex101.js

--- regex101.js
+++ regex101.js
@@ -1,3 +1,3 @@
-Run this to copy all matches to clipboard
+Run this to copy all matches of group 1 to clipboard
 
 copy( [ ...document.querySelectorAll('tr:nth-child(2) > td:nth-child(3)') ].map( i => i.innerText).join("\n") )
