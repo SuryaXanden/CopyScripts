ec2a87d7eeba842beee44f4102eb749a48944c16
Surya U <suryaxanden@gmail.com>
2020-04-20 11:59:52 +0530

    Update jsoneditoronline.js

--- jsoneditoronline.js
+++ jsoneditoronline.js
@@ -1,3 +1,3 @@
 // To copy the selected JSON tree / path, use the following code
 
-copy( document.querySelector("#root > div > div.body > div.contents-panel > div.right-panel > div > div.panel-contents > div > div > div.jsoneditor-navigation-bar > div").innerText.replace(/►/g,'.').replace('object.', '') )
+copy( document.querySelector("#root > div > div.body > div.contents-panel > div.right-panel > div > div.panel-contents > div > div > div.jsoneditor-navigation-bar > div").innerText.replace(/►/g,'.').replace('object.', '').replace(/^...\n/g,'') )
