9903f58ae2ffea21cdbc537496c56ff1ea29d827
Surya U <suryaxanden@gmail.com>
2021-04-20 12:38:17 +0530

    Create datetime_zone.js

---
+++ datetime_zone.js
@@ -0,0 +1,8 @@
+// Use the following code and specify the appropriate time zone
+// To fetch the list of valid timezone strings use this link
+// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
+// for IST
+let now = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
+	
+// let now = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
